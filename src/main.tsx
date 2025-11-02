59e3b26df0490d78d1f6bbabee4079ca442c72d8
Aylin <vahabovaylin@gmail.com>
2025-11-02 12:09:28 +1100

    Suppress harmless browser extension connection errors

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,15 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+// Suppress browser extension connection errors (harmless)
+const originalError = console.error
+console.error = (...args: any[]) => {
+  if (args[0]?.includes?.('Receiving end does not exist') || 
+      args[0]?.includes?.('Could not establish connection')) {
+    // Ignore browser extension errors
+    return
+  }
+  originalError(...args)
+}
+
 createRoot(document.getElementById("root")!).render(<App />);
